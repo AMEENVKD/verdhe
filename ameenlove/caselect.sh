bd576ce43770ef5f0ef06886e76e72cfa59fc376
AMEENIBRAHIM <109067754+AMEENVKD@users.noreply.github.com>
2023-02-06 18:38:59 +0530

    Add files via upload

---
+++ ameenlove/caselect.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "\033[32mchoose ur partner!\033[0m"
+select name in aliya ansha rifan 
+do
+
+     case $name in 
+     aliya)
+        echo aliya selected  jadayilla ellavare ore pole karnum
+        ;;
+    ansha)
+        echo ansha selected  pavarn korch jada korch kuttykali 
+        ;;
+    rifan)
+        echo rifan 404 error found why u selected  she is krimy danger to my health
+        ;;
+      *)
+        echo  "only take from this better 1.2 if u take 3 willl depress your life"
+     esac
+done
+
